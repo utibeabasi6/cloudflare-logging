87d8df3f3e58bc1d80b54bf4b73422d6dbf116d2
Utibeabasi Umanah <utibeabasiumanah6@gmail.com>
2024-06-12 04:05:10 +0100

    fix: add app sg and salt deploy workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "app" {
   }
 
   user_data = file("./scripts/install_salt_minion.sh")
-  vpc_security_group_ids = [ aws_security_group.salt.id ]
+  vpc_security_group_ids = [ aws_security_group.app.id ]
 
   tags = {
     Name = "app-${var.app_region}"
@@ -117,6 +117,32 @@ resource "aws_security_group" "salt" {
     ingress {
     from_port        = 4505
     to_port          = 4506
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "app" {
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+    ingress {
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
